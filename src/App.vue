1e432c682d84e517af106cfaad1a1af9c34bc686
Yevhenns <gifestum@gmail.com>
2025-01-29 12:39:44 +0200

    app refactoring

--- src/App.vue
+++ src/App.vue
@@ -2,62 +2,14 @@
 import { RouterView } from 'vue-router'
 import AppFooter from './components/AppFooter.vue'
 import AppHeader from './components/AppHeader.vue'
+import AppCVButton from './components/AppCVButton.vue'
 </script>
 
 <template>
   <AppHeader />
-
   <RouterView />
-
   <AppFooter />
-  <a
-    class="download"
-    href="/Front-End Developer - Yevhen Ziuskin.pdf"
-    download="Front-End Developer - Yevhen Ziuskin"
-    >Download<br />CV</a
-  >
+  <AppCVButton />
 </template>
 
-<style scoped>
-.download {
-  position: fixed;
-  right: 60px;
-  bottom: 60px;
-  background-color: green;
-  width: 120px;
-  height: 120px;
-  display: flex;
-  text-align: center;
-  align-items: center;
-  justify-content: center;
-  color: #ffffff;
-  border-radius: 50%;
-  -webkit-animation: rotate 4s infinite;
-  animation: rotate 4s infinite;
-  font-size: 20px;
-  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.4);
-  transition: all 250ms linear;
-
-  &:hover {
-    box-shadow: 0 8px 20px rgba(0, 0, 0, 0.7);
-  }
-}
-
-@-moz-keyframes rotate {
-  from {
-    -moz-transform: rotate(0deg);
-  }
-  to {
-    -moz-transform: rotate(360deg);
-  }
-}
-
-@keyframes rotate {
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
-}
-</style>
+<style scoped></style>
---
+++ src/components/AppCVButton.vue
@@ -0,0 +1,52 @@
+<template>
+  <a
+    class="download"
+    href="/Front-End Developer - Yevhen Ziuskin.pdf"
+    download="Front-End Developer - Yevhen Ziuskin"
+    >Download<br />CV</a
+  >
+</template>
+
+<style scoped>
+.download {
+  position: fixed;
+  right: 60px;
+  bottom: 60px;
+  background-color: green;
+  width: 120px;
+  height: 120px;
+  display: flex;
+  text-align: center;
+  align-items: center;
+  justify-content: center;
+  color: #ffffff;
+  border-radius: 50%;
+  -webkit-animation: rotate 4s infinite;
+  animation: rotate 4s infinite;
+  font-size: 20px;
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.4);
+  transition: all 250ms linear;
+
+  &:hover {
+    box-shadow: 0 8px 20px rgba(0, 0, 0, 0.7);
+  }
+}
+
+@-moz-keyframes rotate {
+  from {
+    -moz-transform: rotate(0deg);
+  }
+  to {
+    -moz-transform: rotate(360deg);
+  }
+}
+
+@keyframes rotate {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
